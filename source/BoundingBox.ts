9377d633560eda054ad5f3a99ae19c22c62a2da4
Jason Kuhrt <jason.kuhrt@ssense.com>
2018-03-18 22:16:43 -0400

    fix bounding box module types

--- source/BoundingBox.ts
+++ source/BoundingBox.ts
@@ -1,19 +1,16 @@
-type Position = {
-  x: number
-  y: number
-}
-
-type BoundingBox = Position & {
+type BoundingBox = {
   top: number
   bottom: number
   left: number
   right: number
+  width: number
+  height: number
 }
 
 /**
  * Create a bounding box.
  */
-const make = (width: number, height: number) => ({
+const make = (width: number, height: number): BoundingBox => ({
   top: 0,
   bottom: height,
   left: 0,
@@ -25,12 +22,13 @@ const make = (width: number, height: number) => ({
 /**
  * Update a box's bounds.
  */
-const translate = (x: number, y: number, box: BoundingBox) => ({
-  ...box,
+const translate = (x: number, y: number, box: BoundingBox): BoundingBox => ({
+  width: box.width,
+  height: box.height,
   top: box.top + y,
   bottom: box.bottom + y,
   left: box.left + x,
   right: box.right + x,
 })
 
-export { make, translate }
+export { make, translate, BoundingBox }
