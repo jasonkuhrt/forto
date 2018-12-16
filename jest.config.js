2933f97554f7323deaab1d9cea59483f0ca2a532
Jason Kuhrt <jason.kuhrt@dialogue.co>
2018-12-16 16:45:03 -0500

    test: add typescript type check to ci (#64)
    Closes #61

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  verbose: true,
   preset: "jest-puppeteer-preset",
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
   testRegex: ".*spec.ts$",
   moduleFileExtensions: ["ts", "js"],
-  collectCoverageFrom: ["**/source/**/*.ts", "!**/build/**/*"],
+  collectCoverage: true,
+  collectCoverageFrom: ["**/source/**/*.ts"],
   globals: {
     "ts-jest": {
       diagnostics: false,
+      tsConfig: "tsconfig.spec.json",
     },
   },
 }
